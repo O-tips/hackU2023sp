d53201f09fca3588ac063b24ff6f50d2d83e2714
Ayanyancat <trueme.was.intheschool@gmail.com>
2023-02-18 15:49:50 +0900

    登録フォームをmaterialUIを使って書き換え stackによる縦並びは未実装

--- src/contents/SignUp.jsx
+++ src/contents/SignUp.jsx
@@ -2,17 +2,20 @@ import logo from '../logo.svg';
 import '../App.css';
 import React from 'react';
 import { Button } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+
 import { BrowserRouter, Routes, Route, useNavigate, useLocation, Link} from "react-router-dom";
   
 function SignUp() {
-    return (<>
-    
-    メールアドレス
 
-    パスワード
-
-    </>);
+    return (
+        <> 
+        <h1>ログイン</h1>
+        <TextField id="outlined-basic" label="メールアドレス" variant="outlined" />
+        <TextField id="outlined-basic" label="パスワード" variant="outlined" />
+        <Button variant="contained">ログイン</Button>
+        </>
+ );
 }
 
 export default SignUp;
