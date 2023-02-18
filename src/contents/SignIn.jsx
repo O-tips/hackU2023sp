9d8f6c04d702598e7bd4c1626cdba17e6ca2d480
Ayanyancat <trueme.was.intheschool@gmail.com>
2023-02-18 16:22:02 +0900

    SignInの登録フォームを中央揃え

--- src/contents/SignIn.jsx
+++ src/contents/SignIn.jsx
@@ -1,32 +1,33 @@
 import logo from '../logo.svg';
 import '../App.css';
 import React from 'react';
-import { Button } from '@mui/material';
-import { useForm } from 'react-hook-form';
-
+import { Button,Grid,Box,TextField,Stack } from '@mui/material';
 import { BrowserRouter, Routes, Route, useNavigate, useLocation, Link} from "react-router-dom";
 
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+
+
 function SignIn() {  
-    
-const { register, handleSubmit } = useForm();
-const onSubmit = (data) => console.log(data);
+
     return (
-    <> 
-    <div className="App">
-    <h1>ログイン</h1>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" {...register('email')} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" {...register('password')} type="password" />
-      </div>
-      <button type="submit">ログイン</button>
-    </form>
-  </div>
-  </>
+      <>
+      <h1>ログイン</h1>
+      <Grid container direction="column" alignItems="center">
+      <Box sx={{ width: '70%' }}>
+      <Stack spacing={2}>
+        <TextField id="outlined-basic" label="メールアドレス" variant="outlined" />
+        <TextField id="outlined-basic" label="パスワード" variant="outlined" />
+        <Button variant="contained">ログイン</Button>
+      </Stack>
+    </Box>
+    </Grid>
+      </>
 );
 }
 export default SignIn;
