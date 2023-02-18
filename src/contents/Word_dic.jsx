f0719ca03758a4a50e16e28701c14e0385b4f1fd
Ayanyancat <trueme.was.intheschool@gmail.com>
2023-02-18 19:23:01 +0900

    辞書画面の実装、initial_displayの追加

--- src/contents/Word_dic.jsx
+++ src/contents/Word_dic.jsx
@@ -1,10 +1,55 @@
 import logo from '../logo.svg';
 import '../App.css';
 import React from 'react';
-
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box,Grid } from '@mui/material';
 import { BrowserRouter, Routes, Route, useNavigate, useLocation, Link} from "react-router-dom";
 
 function Word_dic() {
-    return (<>Word_dic</>);
+    return (<>
+    Word_dic
+
+    <div>
+    <Grid container direction="column" alignItems="center">
+    <Box sx={{ width: '70%' }}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>単語1</Typography>
+        </AccordionSummary>
+        <AccordionDetails style={{ color: "#000000", backgroundColor: "#d9d9d9" }}>
+          <Typography>
+            ここに単語の意味を表示
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>単語2</Typography>
+        </AccordionSummary>
+        <AccordionDetails　style={{ color: "#000000", backgroundColor: "#d9d9d9" }}>
+          <Typography>
+            ここに単語の意味を表示
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      </Box>
+      </Grid>
+      
+    </div>
+    
+    
+    </>);
 }
 export default Word_dic;
