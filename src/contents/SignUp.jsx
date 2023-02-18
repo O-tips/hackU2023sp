aee3c7f779988172953e2c4c701d18a379a4fce7
Ayanyancat <trueme.was.intheschool@gmail.com>
2023-02-18 16:01:56 +0900

    SignInの登録フォームをmaterialUIを用いたものに変更

--- src/contents/SignUp.jsx
+++ src/contents/SignUp.jsx
@@ -2,16 +2,14 @@ import logo from '../logo.svg';
 import '../App.css';
 import React from 'react';
 import { Button } from '@mui/material';
-import { useForm } from 'react-hook-form';
 import { BrowserRouter, Routes, Route, useNavigate, useLocation, Link} from "react-router-dom";
   
 function SignUp() {
     return (<>
-    
-    メールアドレス
-
-    パスワード
-
+  <h1>新規登録</h1>
+  <TextField id="outlined-basic" label="メールアドレス" variant="outlined" />
+  <TextField id="outlined-basic" label="パスワード" variant="outlined" />
+  <Button variant="contained">新規登録</Button>
     </>);
 }
 
