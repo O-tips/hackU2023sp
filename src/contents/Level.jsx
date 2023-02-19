3d7f2e7357804d375b61905724852b9fa3bdafba
Ayanyancat <trueme.was.intheschool@gmail.com>
2023-02-19 16:20:01 +0900

    Level.jsxにラジオボタンを追加　VSCode上でエラーが出ているが動作はする

--- src/contents/Level.jsx
+++ src/contents/Level.jsx
@@ -5,11 +5,33 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 
+import { Button,Grid } from '@mui/material';
+
 function Level() {
-    return (<>
-    Level
-    
-    </>);
+    const [value, setValue] = React.useState('level1');
+
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
+  
+    return (
+    <Grid container direction="column" alignItems="center">
+      <FormControl>
+        <FormLabel id="demo-controlled-radio-buttons-group">レベルを選択してください</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="level1" control={<Radio />} label="レベル1" />
+          <FormControlLabel value="level2" control={<Radio />} label="レベル2" />
+          <FormControlLabel value="level3" control={<Radio />} label="レベル3" />
+          <Button variant="outlined">決定</Button>
+        </RadioGroup>
+      </FormControl>
+    </Grid>
+    );
 }
 
 export default Level;
